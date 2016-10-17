a6e53fc91ff1aba2e7b87e7fdddbac2492e80e84
Georgios Fisaris <gfisaris@gmail.com>
2016-10-17 13:24:14 +0100

    Update 314-create.all.appuser.symlinks.sh

--- application.start.cmds/314-create.all.appuser.symlinks.sh
+++ application.start.cmds/314-create.all.appuser.symlinks.sh
@@ -14,17 +14,15 @@ die() {
 echo $PRJGROUP
 echo $PRJUSER
 
-cd ~
-
-ln -s /var/www ~/www
+ln -s /var/www /home/$PRJUSER/www
 
 ln -s /var/www/$PRJGROUP.$PRJUSER/Initial.Deployment/ /var/www/$PRJGROUP.$PRJUSER/live
-ln -s /var/www/$PRJGROUP.$PRJUSER/live/ ~/live
+ln -s /var/www/$PRJGROUP.$PRJUSER/live/ /home/$PRJUSER/live
 
-mkdir -p ~/all.logs/
+mkdir -p /home/$PRJUSER/all.logs/
 
-ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/nginx.access.logs ~/all.logs/nginx.access.logs
-ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/nginx.errors.logs ~/all.logs/nginx.errors.logs
-ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/phpfpm.errors.logs ~/all.logs/phpfpm.errors.logs
-ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/phpfpm.slowreqs.logs  ~/all.logs/phpfpm.slowreqs.logs
-ln -s /var/www/$PRJGROUP.$PRJUSER/live/app/logs/prod.log ~/all.logs/webapp.production.logs
+ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/nginx.access.logs /home/$PRJUSER/all.logs/nginx.access.logs
+ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/nginx.errors.logs /home/$PRJUSER/all.logs/nginx.errors.logs
+ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/phpfpm.errors.logs /home/$PRJUSER/all.logs/phpfpm.errors.logs
+ln -s /var/www/$PRJGROUP.$PRJUSER.vhost.logs/phpfpm.slowreqs.logs  /home/$PRJUSER/all.logs/phpfpm.slowreqs.logs
+ln -s /var/www/$PRJGROUP.$PRJUSER/live/app/logs/prod.log /home/$PRJUSER/all.logs/webapp.production.logs

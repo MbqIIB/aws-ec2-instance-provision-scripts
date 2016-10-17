e5b2bde811a00a55fb571be292b5a91a642ae554
Georgios Fisaris <gfisaris@gmail.com>
2016-10-17 11:01:45 +0100

    Update 303-configure.appuser.aws.crds.sh

--- application.start.cmds/303-configure.appuser.aws.crds.sh
+++ application.start.cmds/303-configure.appuser.aws.crds.sh
@@ -6,9 +6,9 @@ appEco=$(cat /aws.services/.ec2Instance   | grep EcoSystem      | awk '{print $2
 appName=$(cat /aws.services/.ec2Instance  | grep WebApplication | awk '{print $2}')
 
 echo "####################"
-  cat $appEnv
-  cat $appEco
-  cat $appName
+  echo $appEnv
+  echo $appEco
+  echo $appName
 echo "####################"
 
 aws s3 cp s3://$appEnv-$appEco-$appName/.app-iam_user.credentials /aws.services/.aws/.$appEco.$appName.credentials

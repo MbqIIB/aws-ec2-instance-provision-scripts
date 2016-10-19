b5ac19078d9bb3e9da81bf760c51f6453aeafb8a
Georgios Fisaris <gfisaris@gmail.com>
2016-10-19 11:11:51 +0100

    Update 311-sync.appuser.sshd.configuration.sh

--- application.start.cmds/311-sync.appuser.sshd.configuration.sh
+++ application.start.cmds/311-sync.appuser.sshd.configuration.sh
@@ -5,16 +5,18 @@ appEco=$(cat /aws.services/.ec2Instance     | grep EcoSystem        | awk '{prin
 appName=$(cat /aws.services/.ec2Instance    | grep WebApplication   | awk '{print $2}')
 appEnv=$(cat /aws.services/.ec2Instance     | grep Environment      | awk '{print $2}')
 
-# Retrieve GitRepo Information File from S3 Bucket and parse it
-aws s3 cp s3://$appEnv-$appEco-$appName/.app-github.repo /tmp/.app-github.repo
-chmod 600 /tmp/.app-github.repo
-    appGitRepoUsername=$(cat /tmp/.app-github.repo | grep Username      | awk '{print $2}')
-    appGitRepoPassword=$(cat /tmp/.app-github.repo | grep Password      | awk '{print $2}')
-    appGitRepoSSHPrivateKey=$(cat /tmp/.app-github.repo | grep SSHPrivateKey | awk '{print $2}')
-    appGitRepoURL=$(cat /tmp/.app-github.repo      | grep RepoURL       | awk '{print $2}')
-    appGitRepoBranch=$(cat /tmp/.app-github.repo   | grep RepoBranch    | awk '{print $2}')
+# Retrieve GitRepo Information Files from S3 Bucket and parse them
+aws s3 cp s3://$appEnv-$appEco-$appName/github-repository_info /tmp/.app-github-repository_info
+aws s3 cp s3://$appEnv-$appEco-$appName/github-repository_sshprvkey /tmp/.app-github-repository_sshprvkey
+chmod 600 /tmp/.app-github*
+    appGitRepoUsername=$(cat /tmp/.app-github-repository_info | grep Username      | awk '{print $2}')
+    appGitRepoPassword=$(cat /tmp/.app-github-repository_info | grep Password      | awk '{print $2}')
+    appGitRepoURL=$(cat /tmp/.app-github-repository_info      | grep RepoURL       | awk '{print $2}')
+    appGitRepoBranch=$(cat /tmp/.app-github-repository_info   | grep RepoBranch    | awk '{print $2}')    
+    appGitRepoSSHPrivateKey=$(cat /tmp/.app-github-repository_sshprvkey)
+
 # Remove GitRepo Information File from Temp Folder
-rm -rf /tmp/.app-github.repo
+rm -rf /tmp/.app-github*
 
 # Update appGitRepoURL Variable with Git Account Information (username & password)
 appGitRepoURL="$(sed s/github\.com/$appGitRepoUsername\:$appGitRepoPassword\@github\.com/g <<<$appGitRepoURL)"

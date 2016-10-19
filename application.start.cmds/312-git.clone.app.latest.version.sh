45443f18de87362abd50130143ee302b6c2a4d71
Georgios Fisaris <gfisaris@gmail.com>
2016-10-19 11:38:45 +0100

    Update 312-git.clone.app.latest.version.sh

--- application.start.cmds/312-git.clone.app.latest.version.sh
+++ application.start.cmds/312-git.clone.app.latest.version.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-#GET Project's Name and EcoSystem
-appEco=$(cat /aws.services/.ec2Instance     | grep EcoSystem        | awk '{print $2}')
-appName=$(cat /aws.services/.ec2Instance    | grep WebApplication   | awk '{print $2}')
-appEnv=$(cat /aws.services/.ec2Instance     | grep Environment      | awk '{print $2}')
-
-# Retrieve GitRepo Information Files from S3 Bucket and parse them
-aws s3 cp s3://$appEnv-$appEco-$appName/github-repository_info /tmp/.app-github-repository_info
-aws s3 cp s3://$appEnv-$appEco-$appName/github-repository_sshprvkey /tmp/.app-github-repository_sshprvkey
-chmod 600 /tmp/.app-github*
-    appGitRepoUsername=$(cat /tmp/.app-github-repository_info | grep Username      | awk '{print $2}')
-    appGitRepoPassword=$(cat /tmp/.app-github-repository_info | grep Password      | awk '{print $2}')
-    appGitRepoURL=$(cat /tmp/.app-github-repository_info      | grep RepoURL       | awk '{print $2}')
-    appGitRepoBranch=$(cat /tmp/.app-github-repository_info   | grep RepoBranch    | awk '{print $2}')    
-    appGitRepoSSHPrivateKey=$(cat /tmp/.app-github-repository_sshprvkey)
-
-# Remove GitRepo Information File from Temp Folder
-rm -rf /tmp/.app-github*
-
-# Update appGitRepoURL Variable with Git Account Information (username & password)
-appGitRepoURL="$(sed s/github\.com/$appGitRepoUsername\:$appGitRepoPassword\@github\.com/g <<<$appGitRepoURL)"
-
 #ERROR Handing Function
 die() {
     echo "FATAL ERROR: $* (status $?)" 1>&2
     echo "Aborting Deployment.." 1>&2
     exit 1
 }
+
+#GET Project's Name and EcoSystem
+appEco=$(cat /aws.services/.ec2Instance     | grep EcoSystem        | awk '{print $2}')
+appName=$(cat /aws.services/.ec2Instance    | grep WebApplication   | awk '{print $2}')
+appEnv=$(cat /aws.services/.ec2Instance     | grep Environment      | awk '{print $2}')
+
+# Retrieve GitHub Repo Configuration Files from S3 Bucket
+    aws s3 cp s3://$appEnv-$appEco-$appName/github-repository_info /tmp/.app-github-repository_info
+    chmod 600 /tmp/.app-github*
+
+# Parse GitHub Repo Information
+    appGitRepoUsername=$(cat /tmp/.app-github-repository_info | grep Username      | awk '{print $2}')
+    appGitRepoPassword=$(cat /tmp/.app-github-repository_info | grep Password      | awk '{print $2}')
+    appGitRepoURL=$(cat /tmp/.app-github-repository_info      | grep RepoURL       | awk '{print $2}')
+    appGitRepoBranch=$(cat /tmp/.app-github-repository_info   | grep RepoBranch    | awk '{print $2}')    
+
+# Remove GitRepo Information File from Temp Folder
+    rm -rf /tmp/.app-github*
+
+
+# Update appGitRepoURL Variable with Git Account Information (username & password)
+appGitRepoURL="$(sed s/github\.com/$appGitRepoUsername\:$appGitRepoPassword\@github\.com/g <<<$appGitRepoURL)"
 
 
 git clone -b $appGitRepoBranch $appGitRepoURL /var/www/$appEco.$appName/Initial.Deployment/
